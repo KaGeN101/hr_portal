305e4490d848cae3d8c61152d63f07f65e7c3310
joee <kagen101@gmail.com>
2019-03-01 15:29:48 +0200

    Employee with CRUD done and all working

--- lib/hr_portal_web/controllers/employee_controller.ex
+++ lib/hr_portal_web/controllers/employee_controller.ex
@@ -8,13 +8,13 @@ defmodule HrPortalWeb.EmployeeController do
     render(conn, "index.html", employees: employees)
   end
 
-  #def new(conn, _params) do
-  #  changeset = HrHub.change_employee(%Employee{})
-  #  render(conn, "new.html", changeset: changeset)
-  #end
+  def new(conn, _params) do
+    changeset = Employees.change_employee(%HumanResources.Employee{})
+    render(conn, "new.html", changeset: changeset)
+  end
 
   def create(conn, %{"employee" => employee_params}) do
-    case Employees.create(employee_params) do
+    case Employees.create_employee(employee_params) do
       {:ok, employee} ->
         conn
         |> put_flash(:info, "Employee created successfully.")
@@ -25,37 +25,37 @@ defmodule HrPortalWeb.EmployeeController do
     end
   end
 
-  #def show(conn, %{"id" => id}) do
-  #  employee = HrHub.get_employee!(id)
-  #  render(conn, "show.html", employee: employee)
-  #end
+  def show(conn, %{"id" => id}) do
+    employee = Employees.get_employee!(id)
+    render(conn, "show.html", employee: employee)
+  end
 
-  #def edit(conn, %{"id" => id}) do
-  #  employee = HrHub.get_employee!(id)
-  #  changeset = HrHub.change_employee(employee)
-  #  render(conn, "edit.html", employee: employee, changeset: changeset)
-  #end
+  def edit(conn, %{"id" => id}) do
+    employee = Employees.get_employee!(id)
+    changeset = Employees.change_employee(employee)
+    render(conn, "edit.html", employee: employee, changeset: changeset)
+  end
 
-  #def update(conn, %{"id" => id, "employee" => employee_params}) do
-  #  employee = HrHub.get_employee!(id)
+  def update(conn, %{"id" => id, "employee" => employee_params}) do
+    employee = Employees.get_employee!(id)
 
-  # case HrHub.update_employee(employee, employee_params) do
-  #    {:ok, employee} ->
-  #      conn
-  #      |> put_flash(:info, "Employee updated successfully.")
-  #      |> redirect(to: Routes.employee_path(conn, :show, employee))
+    case Employees.update_employee(employee, employee_params) do
+      {:ok, employee} ->
+        conn
+        |> put_flash(:info, "Employee updated successfully.")
+        |> redirect(to: Routes.employee_path(conn, :show, employee))
 
-  #    {:error, %Ecto.Changeset{} = changeset} ->
-  #      render(conn, "edit.html", employee: employee, changeset: changeset)
-  #  end
-  #3end
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", employee: employee, changeset: changeset)
+    end
+  end
 
-  #def delete(conn, %{"id" => id}) do
-  #  employee = HrHub.get_employee!(id)
-  #  {:ok, _employee} = HrHub.delete_employee(employee)
+  def delete(conn, %{"id" => id}) do
+    employee = Employees.get_employee!(id)
+    {:ok, _employee} = Employees.delete_employee(employee)
 
-  #  conn
-  #  |> put_flash(:info, "Employee deleted successfully.")
-  #  |> redirect(to: Routes.employee_path(conn, :index))
-  #end
+    conn
+     |> put_flash(:info, "Employee deleted successfully.")
+     |> redirect(to: Routes.employee_path(conn, :index))
+  end
 end
